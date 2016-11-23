4fd3f57bd91a176750fdae40c0dd62010020380a
icarusvolans <icaruswalking@gmail.com>
2016-11-22 19:44:55 -0700

    Add files via upload

---
+++ Furniture-master/src/com/Accessories/LightBulb.java
@@ -0,0 +1,13 @@
+package com.Accessories;
+
+import com.company.Furniture;
+
+public class LightBulb {
+
+    public void getMaterialType() {
+        Furniture f = new Furniture();
+        f.typeOfMaterial();
+
+    }
+
+}
---
+++ Furniture-master/src/com/company/Furniture.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Furniture {
+
+//    private String material = "wood";
+    public String material = "wood";
+
+//    private void typeOfMaterial() {
+    public void typeOfMaterial() {
+        System.out.println(material);
+    }
+
+    private void getMaterial() {
+        typeOfMaterial();
+    }
+}
